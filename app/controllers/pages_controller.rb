c2799f5ba542109ebe80466437066bc928de49f5
Jono <jono@platform45.com>
2015-12-03 15:52:40 +0200

    fix: track social activity on views only :wrench:

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,9 +5,13 @@ class PagesController < ApplicationController
 
   def stat
     begin
-      page.track_social_activity if page.updated_at > 5.minutes.ago
       page.social_activities.create(social_activity)
       page.update(score: page.calculated_score)
+
+      if params[:stat_type] == 'page_view' && page.updated_at > 5.minutes.ago
+        page.track_social_activity
+      end
+
       render json: page, status: :created
     rescue
       render json: {}, status: :unprocessible_entity
